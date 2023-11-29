ad6d730acf688df4ab05466c9839b9753bbbd68d
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-11-29 16:52:26 +0000

    Binary tree full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,15 +10,15 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left != NULL)
+	if (tree->left != NULL && tree->right != NULL)
+	{
 		decision = binary_tree_is_full(tree->left);
-	if (tree->right != NULL)
 		decision = binary_tree_is_full(tree->right);
+	}
 	if ((tree->left != NULL && tree->right == NULL) ||
 	(tree->right != NULL && tree->left == NULL))
 	{
 		return (0);
-		exit(0);
 	}
 	return (decision);
 }
